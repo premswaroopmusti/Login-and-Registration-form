d7527f61d9f42ed0fadececae39b6295c22b729a
Prem <premswaroopmusti@gmail.com>
2022-12-13 11:52:04 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/loginandregistrationform/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.loginandregistrationform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView btn = findViewById(R.id.textviewsignup);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
